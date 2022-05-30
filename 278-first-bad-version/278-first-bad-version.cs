23ee4fe1ca720fa88ccdb2d6fa69ad31043e0251
Melody Magdy Wafik <86573087+magdy-wafik@users.noreply.github.com>
2022-05-30 02:37:17 +0200

    Time: 40 ms (25.67%), Space: 25.5 MB (36.59%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cs
+++ 278-first-bad-version/278-first-bad-version.cs
@@ -4,28 +4,15 @@
 public class Solution : VersionControl {
     public int FirstBadVersion(int n) {
         
-        if(n == 1 && IsBadVersion(n) )
-            return n;
-            
-       int middleIndex = n/2;
-                
-        for(int i = 1 ; i< middleIndex ; i++)
-        {
-            if(IsBadVersion(i))
-               return i; 
-            
+        int start = 1;
+       while(start < n)
+       {
+           int middleValue = start + (n- start)/2;
+            if(IsBadVersion(middleValue))
+                n = middleValue;
+            else
+                start = middleValue + 1;
         }
-        
-         if(IsBadVersion(middleIndex))
-            return middleIndex;
-        
-        for(int i = middleIndex+1 ; i <= n ; i++)
-        {
-            if(IsBadVersion(i))
-               return i; 
-            
-        } 
-        
-        return -1;
+            return start;
+       }
     }
-}
