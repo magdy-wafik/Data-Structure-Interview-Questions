dc475924fb9aa02995a7e38dfdf27d8d1dc0cbb8
Melody Magdy Wafik <86573087+magdy-wafik@users.noreply.github.com>
2022-09-10 19:29:26 +0200

    Time: 400 ms (14.47%), Space: 49.3 MB (22.49%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,18 +1,21 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-        int sum = 0;
-        int Maxsum = nums[0];
-        for(int i = 0 ; i < nums.Length ; i++)
+        int currentSum = 0;
+        int LargestSum = nums[0];
+        for (int i = 0 ; i < nums.Length ; i++)
         {
-            sum += nums[i];  
+            currentSum += nums[i];
+            if(currentSum < nums[i])
+            {
+                currentSum = nums[i];
+            }
             
-            if(sum < nums[i])
-                sum = nums[i];
-            
-            if(Maxsum < sum)
-                Maxsum = sum;
+            if(LargestSum < currentSum)
+            {
+             LargestSum = currentSum ;   
+            }
             
         }
-        return Maxsum;
+        return LargestSum;
     }
 }
